6cdbbb067f4e999e70eb54632557a00764514768
Mr-ASbaloch <abdulsaeed1356@gmail.com>
2023-08-22 05:25:03 -0700

    commit

--- antdesign/src/Components/UseButton.jsx
+++ antdesign/src/Components/UseButton.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button } from "antd";
+import { Button  , FloatButton } from "antd";
 import {GrSubtractCircle} from 'react-icons/gr'
 
 
@@ -10,9 +10,24 @@ const UseButton = () => {
 
       <Button type="dashed">first button</Button>
       <Button type="text">first button</Button>
-      <Button type="default" loading  htmlType="button" placeholder="" icon={<GrSubtractCircle/>}    >
+      <Button type="default" loading  htmlType="button"  icon={<GrSubtractCircle/>}    >
         button
       </Button>
+
+
+
+      <FloatButton.Group shape="circle" style={{right:20}} >
+      <FloatButton  shape="square" tooltip={<div>document</div>} >
+        submit
+      </FloatButton>
+      <FloatButton  shape="square" tooltip={<div>document</div>} >
+        submit
+      </FloatButton>
+      <FloatButton  shape="square" tooltip={<div>document</div>} >
+        submit
+      </FloatButton>
+      </FloatButton.Group>
+
 
     </div>
   );
